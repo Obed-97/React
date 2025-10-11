ed3d2a6757a4dce0c926319d8b71be0bdcb96a49
Obed <djekounyom6@gmail.com>
2025-10-11 14:38:00 +0200

    test commit

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
 
-import './App.css'
+import React from 'react';
 
 function App() {
- 
-
   return (
-    <>
-      
-      <h1>First counter</h1>
-      
-    </>
-  )
+    <div className="App">
+      <h1>Bonjour, React!</h1>
+    </div>
+  
+  );
 }
 
-export default App
+export default App;
